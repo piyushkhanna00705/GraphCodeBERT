b1af4f9e53fb128ae0bf14def9aede6420c98154
piyushkhanna00705 <piyushkhanna00705@gmail.com>
2023-11-14 18:00:11 -0500

    Inference on test set

---
+++ GraphCodeBERT/translation/inference.sh
@@ -0,0 +1,28 @@
+source=java
+target=cs
+beam_size=10
+source_length=320
+target_length=256
+output_dir=saved_models/$source-$target/
+pretrained_model=microsoft/graphcodebert-base
+
+batch_size=64
+dev_file=data/valid.java-cs.txt.$source,data/valid.java-cs.txt.$target
+test_file=data/test.java-cs.txt.$source,data/test.java-cs.txt.$target
+load_model_path=$output_dir/checkpoint-best-bleu/pytorch_model.bin #checkpoint for test
+
+python run.py \
+--do_test \
+--model_type roberta \
+--source_lang $source \
+--model_name_or_path $pretrained_model \
+--tokenizer_name microsoft/graphcodebert-base \
+--config_name microsoft/graphcodebert-base \
+--load_model_path $load_model_path \
+--dev_filename $dev_file \
+--test_filename $test_file \
+--output_dir $output_dir \
+--max_source_length $source_length \
+--max_target_length $target_length \
+--beam_size $beam_size \
+--eval_batch_size $batch_size 2>&1| tee $output_dir/test.log
